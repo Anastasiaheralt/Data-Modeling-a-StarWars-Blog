af32b2bf06fa309b14246b8b8f11bce161762d09
Anastasiaheralt <rosimarherrerao@gmail.com>
2022-09-14 02:57:47 +0000

    diagrama generado

--- src/models.py
+++ src/models.py
@@ -8,58 +8,41 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Comment(Base):
-    __tablename__ = 'comment'
-    
-    id = Column(Integer, primary_key=True)
-    comment_text = Column(String(250))
-    author_id = Column(Integer, ForeignKey('user.id'))
-    post_id = Column(Integer, ForeignKey('post.id'))
-
-class Media(Base):
-    __tablename__ = 'media'
-
-    id = Column(Integer, primary_key=True)
-    type = Column(String(250), nullable=False)
-    url = Column(String(250), nullable=False)
-    post_id = Column(Integer, ForeignKey('post.id')) 
-
-class Post(Base):
-    __tablename__ = 'post'
-    
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id')) 
-    comment = relationship(Comment)
-    media = relationship(Media)
-
-class Follower(Base):
-    __tablename__ = 'follower'
-
-    id = Column(Integer, primary_key=True)
-    user_from_id = Column(Integer, ForeignKey('user.id'))
-    user_to_id =  Column(Integer, ForeignKey('user.id'))
-
-class User(Base):
+class Users(Base):
     __tablename__ = 'user'
-  
     id = Column(Integer, primary_key=True)
-    username = Column(String(250), nullable=False)
-    firtsname = Column(String(250), nullable=False)
-    lastname = Column(String(250), nullable=False)
+    name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
     password = Column(String(250), nullable=False)
-    post = relationship(Post)
-    comment = relationship(Comment)
-    media = relationship(Media)
-    follower = relationship(Follower)
+   
+class FavoritePlanets(Base):
+    __tablename__ = 'fav_planet'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    user = relationship(Users)
 
+class FavoriteCharacters(Base):
+    __tablename__ = 'fav_character'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    character_id = Column(Integer, ForeignKey('character.id'))
+    user = relationship(Users)
+
+class Planets(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=False)
+
+class Characters(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=False)
+    
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise 
+render_er(Base, 'diagram.png')
